2dc8dce855dfc391c87a0cecb318d6223e3f9d7c
Adrián Ribao Martínez <adrian@adrima.es>
2013-05-07 17:54:00 +0200

    use class based generic views

--- django_users/backends/confirmationemail/urls.py
+++ django_users/backends/confirmationemail/urls.py
@@ -1,6 +1,6 @@
 #coding=utf-8
 from django.conf.urls.defaults import *
-from django.views.generic.simple import direct_to_template
+from django.views.generic import TemplateView
 from django_users.urls import urlpatterns as baseurlpatterns
 from django_users.backends.confirmationemail.views import Activate, CreateUser
 from django_users.forms import CreateUserForm
@@ -11,8 +11,7 @@ urlpatterns = patterns('',
             name='register'
             ),
         url(r'^activation-sent/$',
-            direct_to_template,
-            {'template':'users/confirmationemail/activation-sent.html',},
+            TemplateView.as_view(template_name='users/confirmationemail/activation-sent.html'),
             name='activation-sent'
             ),
         url(r'^activate/(.+)/$',
